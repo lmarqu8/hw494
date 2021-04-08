e41c452850fe014f76954fa58fd0ffa36804fb15
lmarqu8 <46792388+lmarqu8@users.noreply.github.com>
2021-04-07 20:18:28 -0500

    dump

---
+++ cmake-build-debug/CMakeFiles/hw494.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw494.dir/hw494/hw494.cpp.obj"
+  "hw494.exe"
+  "hw494.exe.manifest"
+  "hw494.pdb"
+  "libhw494.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw494.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
